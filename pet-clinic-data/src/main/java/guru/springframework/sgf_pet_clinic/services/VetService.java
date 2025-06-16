2cfb9cfbaba7ed52eb40fb31a7ef2eeec6564d3d
Maxime LEBLANC <leblanc.maxime49@hotmail.fr>
2025-06-16 15:16:34 +0200

    Implement services

--- pet-clinic-data/src/main/java/guru/springframework/sgf_pet_clinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sgf_pet_clinic/services/VetService.java
@@ -5,7 +5,6 @@ import guru.springframework.sgf_pet_clinic.model.Vet;
 import java.util.Set;
 
 public interface VetService {
-    Vet findByLastName(String lastName);
     Vet findById(Long id);
     Vet save(Vet vet);
     Set<Vet> findAll();
