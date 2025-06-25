a21e73d759c7435d35d66ad4057f7939c990e647
Maxime LEBLANC <leblanc.maxime49@hotmail.fr>
2025-06-25 16:43:04 +0200

    Added PetTypeService

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfg_pet_clinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfg_pet_clinic.services;
+
+import guru.springframework.sfg_pet_clinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
