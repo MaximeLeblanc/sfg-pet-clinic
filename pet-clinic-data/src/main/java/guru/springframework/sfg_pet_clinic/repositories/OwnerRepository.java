d75c9231a8faacb6d0c14596ba174ba4127e0e78
Maxime LEBLANC <leblanc.maxime49@hotmail.fr>
2025-06-27 17:10:55 +0200

    Implement spring data jpa service of owners

--- pet-clinic-data/src/main/java/guru/springframework/sfg_pet_clinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfg_pet_clinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import guru.springframework.sfg_pet_clinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
