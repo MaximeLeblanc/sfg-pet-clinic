d04e3161d7c8cd6e800434f5211d7062a381578b
Maxime LEBLANC <leblanc.maxime49@hotmail.fr>
2025-07-01 11:06:51 +0200

    Fix VisitMapService with service annotation

--- pet-clinic-data/src/main/java/guru/springframework/sfg_pet_clinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfg_pet_clinic/services/map/VisitMapService.java
@@ -2,9 +2,11 @@ package guru.springframework.sfg_pet_clinic.services.map;
 
 import guru.springframework.sfg_pet_clinic.model.Visit;
 import guru.springframework.sfg_pet_clinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
     @Override
     public Set<Visit> findAll() {
