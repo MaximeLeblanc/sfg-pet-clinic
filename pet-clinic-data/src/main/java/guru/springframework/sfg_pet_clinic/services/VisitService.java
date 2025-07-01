0e6a02b4e1ebf67baff07c6f6b3fdf8b8e7e3118
Maxime LEBLANC <leblanc.maxime49@hotmail.fr>
2025-07-01 11:05:17 +0200

    Implements Map impl for Visit service

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfg_pet_clinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfg_pet_clinic.services;
+
+import guru.springframework.sfg_pet_clinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
